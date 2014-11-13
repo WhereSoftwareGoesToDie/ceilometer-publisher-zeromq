120a84c703d6d3838ad1b02e14bed1bba02863af
Sharif Olorin <sio@tesser.org>
2014-11-13 17:12:40 +1100

    Wrap to appropriate line width

--- ceilometer_publisher_rabbitmq/queue.py
+++ ceilometer_publisher_rabbitmq/queue.py
@@ -17,51 +17,40 @@ OPTS = [
                default='/'),
     cfg.StrOpt('publisher_exchange',
                default='publisher-exchange',
-               help='The exchange to use for publishing samples')
+               help='The exchange to use for publishing samples'),
+    cfg.StrOpt('publisher_queue',
+               default='publisher-queue',
+               help='The queue to use for publishing samples'),
 ]
 
 cfg.CONF.register_opts(OPTS)
-
-def logOkExchange(exchange):
-    """Callback on Exchange.DeclareOk"""
-    def writeLog():
-        LOG.info("Declared exchange %s" % exchange)
-    return writeLog
-
-def logOkQueue(queue):
-    """Callback on Queue.DeclareOk"""
-    def writeLog():
-        LOG.info("Declared queue %s" % queue)
-    return writeLog
-
-def logOkBind(exchange, queue):
-    """Callback on Queue.BindOk"""
-    def writeLog():
-        LOG.info("Queue %s bound to exchange %s" % (queue, exchange))
-    return writeLog
 
 class QueuePublisher(publisher.PublisherBase):
     """Republishes all received samples to a rabbit queue"""
 
     def __init__(self, parsed_url):
         super(QueuePublisher, self).__init__(parsed_url)
-        credentials = pika.credentials.PlainCredentials(username = cfg.CONF.publisher_rabbit_user,
-                                                        password = cfg.CONF.publisher_rabbit_password,
-                                                        erase_on_connect = True)
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=cfg.CONF.publisher_rabbit_host,
-                                                                            port=cfg.CONF.publisher_rabbit_port,
-                                                                            virtual_host=cfg.CONF.publisher_rabbit_virtual_host,
-                                                                            credentials=credentials))
+        credentials = pika.credentials.PlainCredentials(
+            username = cfg.CONF.publisher_rabbit_user,
+            password = cfg.CONF.publisher_rabbit_password,
+            erase_on_connect = True,
+        )
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=cfg.CONF.publisher_rabbit_host,
+                port=cfg.CONF.publisher_rabbit_port,
+                virtual_host=cfg.CONF.publisher_rabbit_virtual_host,
+                credentials=credentials,
+            )
+        )
         self.channel = self.connection.channel()
         self.exchange = cfg.CONF.publisher_exchange
         queue = cfg.CONF.publisher_queue
-        self.channel.exchange_declare(logOkExchange(self.exchange),
-                                      exchange=self.exchange,
+        self.channel.exchange_declare(exchange=self.exchange,
                                       type='fanout')
-        self.channel.queue_declare(logOkQueue(queue), queue=queue,
+        self.channel.queue_declare(queue=queue,
                                    durable=True, auto_delete=False)
-        self.channel.queue_bind(logOkBind(self.exchange, queue),
-                                queue=queue,
+        self.channel.queue_bind(queue=queue,
                                 exchange=self.exchange)
 
     def publish_samples(self, context, samples):
