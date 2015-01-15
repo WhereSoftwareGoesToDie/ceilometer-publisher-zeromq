cad4c6f4119a2ebe80cd9dec6574ae4091573f84
Oswyn Brent <oswyn.brent@anchor.com.au>
2015-01-15 15:47:36 +1100

    Revert "updated imports from ceilometer"
    This reverts commit afbc7d735e2f5da2175dc33314b0aa5b2bc96fe1.

--- ceilometer_publisher_rabbitmq/queue.py
+++ ceilometer_publisher_rabbitmq/queue.py
@@ -1,6 +1,7 @@
 from ceilometer import publisher
+from ceilometer.openstack.common.gettextutils import _
 from ceilometer.openstack.common import log
-from oslo_config import cfg
+from oslo.config import cfg
 from time import sleep
 import json
 import pika
@@ -86,4 +87,4 @@ class QueuePublisher(publisher.PublisherBase):
                 LOG.warning("Failed to publish message, sleeping 3 seconds then reconnecting")
                 sleep(3)
                 self.reconnect()
-            LOG.debug(("Queue Publisher published %s to exchange %s") % (message, self.exchange))
+            LOG.debug(_("Queue Publisher published %s to exchange %s") % (message, self.exchange))
